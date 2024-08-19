a46dba761fc711b15dc6d0995b3eda935d685056
mf0zz13 <michael.foster31@outlook.com>
2024-08-19 11:57:48 -0400

    Created week seven assignment template

---
+++ Assignments/Week_7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WeekSevenAssignments
+{
+    private class Program
+    { 
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
