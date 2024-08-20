d3f03f8fd6b6ed4f433405a67fe8e3655b510438
mf0zz13 <michael.foster31@outlook.com>
2024-08-20 00:10:18 -0400

    Created template class for seven three and four

---
+++ Assignments/Week_7/AssignmentSevenFour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace WeekSevenAssignments
+{
+    internal static class AssignmentSevenFour
+    {
+        public static void SevenFour()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignments/Week_7/AssignmentSevenThree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekSevenAssignments
+{
+    internal static class AssignmentSevenThree
+    {
+        public static void SevenThree()
+        {
+            Console.ReadKey();
+        }
+    }
+}
