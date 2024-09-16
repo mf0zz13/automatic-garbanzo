a373f12f2ef31e5e8a605040747922ebbd7a81ec
mf0zz13 <michael.foster31@outlook.com>
2024-09-16 09:48:07 -0400

    Week 11 challenges part two

---
+++ Assignments/Week_11/11_Challenges/Part_Two/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Weel11ChallengeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindBallon("nlaebolko"));
+            Console.WriteLine(FindBallon("loonbalxballpoon"));
+            Console.ReadKey();
+        }
+
+        static int FindBallon(string input)
+        {
+            Dictionary<char, int> letterCount = new();
+
+            foreach (char letter in input.ToLower())
+            {
+                if (letterCount.ContainsKey(letter))
+                {
+                    letterCount[letter] += 1;
+                }
+                else
+                {
+                    letterCount.Add(letter, 1);
+                }
+            }
+
+            int maxInstances = 0;
+            foreach (char letter in "balon")
+            {
+                switch (letter)
+                {
+                    case 'b':
+                        if (!letterCount.ContainsKey(letter))
+                            return 0;
+                        maxInstances = letterCount[letter];
+                        break;
+                    case 'a':
+                    case 'n':
+                        if (!letterCount.ContainsKey(letter))
+                            return 0;
+                        if (letterCount[letter] < maxInstances)
+                            maxInstances = letterCount[letter];
+                        break;
+                    case 'l':
+                    case 'o':
+                        if (!letterCount.ContainsKey(letter))
+                            return 0;
+                        int tempMaxInstances = Convert.ToInt32(Math.Floor((double)letterCount[letter] / 2.0));
+                        if (tempMaxInstances < maxInstances)
+                            maxInstances = tempMaxInstances;
+                        break;
+                }
+            }
+
+            return maxInstances;
+        }
+    }
+}
