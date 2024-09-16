03925fa373b242bfd91c2b5c0d7a3c69ee835c86
mf0zz13 <michael.foster31@outlook.com>
2024-09-16 10:12:14 -0400

    Week 11 challenge part one completed

---
+++ Assignments/Week_11/11_Challenges/Part_One/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Week11ChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] colors = [2, 0, 2, 1, 1, 0];
+
+            ColorSort(colors);
+
+            foreach (int num in colors)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static void ColorSort(int[] colors)
+        {
+            for (int i = 1; i < colors.Length; i++)
+            {
+                int temp = colors[i];
+                int position = i;
+                while (position > 0 && colors[i - 1] > temp)
+                {
+                    colors[position] = colors[position - 1];
+                    position--;
+                }
+                colors[position] = temp;
+            }
+        }
+
+
+    }
+}
