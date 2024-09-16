e7e318f9a4b5f7b0e2cf0597512d7c5747b742b5
mf0zz13 <michael.foster31@outlook.com>
2024-09-15 23:52:48 -0400

    Added Week 10 Challenges

---
+++ Assignments/Week_10/Challenges/Challenge One/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace WeekTenChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = [1, 2, 3, 5];
+            int[] ages = [8, 9, 10, 1];
+
+            Console.WriteLine(HighScore(scores, ages));
+            Console.ReadKey();
+
+        }
+
+        static int HighScore(int[] scores, int[] ages)
+        {
+            List<(int scores, int dp)> players = new();
+
+            for (int i = 0; i < scores.Length; i++)
+            {
+                players.Add((ages[i], scores[i]));
+            }
+
+            players = players.OrderBy(player => player.dp)
+                .ThenBy(player => player.scores)
+                .Select(player => (player.scores, player.scores))
+                .ToList<(int scores, int dp)>();
+
+            for (int i = 1; i < players.Count; i++)
+            {
+                foreach ((int score, int dp) player in players[..i])
+                {
+                    if (player.score <= players[i].scores)
+                    {
+                        players[i].dp = Math.Max(player.dp + players[i].scores, players[i].dp);
+                    }
+                }
+            }
+            return players.Max(x => x.dp);
+        }
+    }
+}
---
+++ Assignments/Week_10/Challenges/Challenge Two/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Week10ChallengeTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = new int[] { 2, 5, 6 };
+            int m = 3;
+            int n = 3;
+
+            MergeArray(nums1, m, nums2, n);
+            foreach (int num in nums1)
+            {
+                Console.Write(num + " ");
+            }
+            Console.Read();
+        }
+
+        static void MergeArray(int[] nums1, int m, int[] nums2, int n)
+        {
+            int p1 = m - 1;
+            int p2 = n - 1;
+
+            if (n == 0)
+                return;
+            if (m == 0)
+            {
+                nums1 = nums2;
+                return;
+            }
+            for (int i = nums1.Length - 1; i >= 0; i--)
+            {
+                if (p2 < 0)
+                    return;
+
+                if (p1 >= 0 && nums1[p1] > nums2[p2])
+                {
+                    nums1[i] = nums1[p1];
+                    p1--;
+                }
+                else
+                {
+                    nums1[i] = nums2[p2];
+                    p2--;
+                }
+            }
+        }
+    }
+}
